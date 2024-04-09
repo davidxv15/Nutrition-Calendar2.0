eeda8e6f4105dab318ef574797eaf3a1cef8d104
davidxv15 <davidxvaz@gmail.com>
2024-04-08 17:26:28 -0700

    entries rendering

--- src/components/Calendar/DayModal.jsx
+++ src/components/Calendar/DayModal.jsx
@@ -3,21 +3,47 @@ import React, { useState } from 'react';
 const DayModal = ({ day, entries, setEntries, closeModal }) => {
   const [foodName, setFoodName] = useState('');
   const [calories, setCalories] = useState('');
+  console.log(entries[day]); 
+
 
   const addEntry = () => {
+    if (!foodName || calories <= 0) {
+      alert('Please fill in all fields correctly.');
+      return;
+    }
     const newEntry = { foodName, calories: parseInt(calories, 10) };
     const dayEntries = entries[day] ? [...entries[day], newEntry] : [newEntry];
     setEntries({ ...entries, [day]: dayEntries });
-    closeModal();
+    setFoodName(''); // Reset the input fields after adding an entry
+    setCalories('');
+  };
+
+  // Function to list out the entries for the selected day
+  const renderDayEntries = (dayEntries) => {
+    return dayEntries.map((entry, index) => (
+      <li key={index}>
+        {entry.foodName} - {entry.calories} Calories
+      </li>
+    ));
   };
 
   return (
-    <div style={{ position: 'fixed', top: '20%', left: '50%', transform: 'translate(-50%, -50%)', backgroundColor: 'white', padding: '20px' }}>
-      <h3>Add Entry for Day {day}</h3>
-      <input type="text" placeholder="Food Name" value={foodName} onChange={(e) => setFoodName(e.target.value)} />
-      <input type="number" placeholder="Calories" value={calories} onChange={(e) => setCalories(e.target.value)} />
-      <button onClick={addEntry}>Add Entry</button>
-      <button onClick={closeModal}>Close</button>
+    <div style={{ position: 'fixed', top: '20%', left: '50%', transform: 'translate(-50%, -50%)', backgroundColor: 'burlywood', padding: '20px', zIndex: 1000 }}>
+      <h3>Entries for Day {day}</h3>
+      <div>
+        {entries[day] && entries[day].length > 0 ? (
+          <ul>{renderDayEntries(entries[day])}</ul>
+        ) : (
+          <p>No entries for this day.</p>
+        )}
+      </div>
+      <hr />
+      <div>
+        <input type="text" placeholder="Food Name" value={foodName} onChange={(e) => setFoodName(e.target.value)} />
+        <input type="number" placeholder="Calories" value={calories} onChange={(e) => setCalories(e.target.value)} />
+        <button onClick={addEntry}>Add Entry</button>
+      </div>
+      <button onClick={closeModal} style={{marginTop: "10px"}}>Close</button>
     </div>
   );
 };
