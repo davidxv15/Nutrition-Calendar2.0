147faa9ee2efd99dcff45ae613ffc7f173abe48a
davidxv15 <davidxvaz@gmail.com>
2024-04-08 21:53:43 -0700

    delete button

--- src/components/Calendar/DayModal.jsx
+++ src/components/Calendar/DayModal.jsx
@@ -1,27 +1,42 @@
 import React, { useState } from "react";
+import { IoMdCloseCircle } from "react-icons/io";
+import { BsTrash3 } from "react-icons/bs";
 
 const DayModal = ({ day, entries, setEntries, closeModal }) => {
   const [foodName, setFoodName] = useState("");
   const [calories, setCalories] = useState("");
   console.log(entries[day]);
 
+  const totalCalories = entries[day] ? entries[day].reduce((total, entry) => total + entry.calories, 0) : 0;
+
+
   const addEntry = () => {
     if (!foodName || calories <= 0) {
-      alert("Please fill in all fields correctly.");
+      alert("Fields may not be empty");
       return;
     }
     const newEntry = { foodName, calories: parseInt(calories, 10) };
     const dayEntries = entries[day] ? [...entries[day], newEntry] : [newEntry];
     setEntries({ ...entries, [day]: dayEntries });
-    setFoodName(""); // Reset the input fields after adding an entry
+    setFoodName(""); // Resets to empty
     setCalories("");
   };
 
-  // Function to list out the entries for the selected day
+  const deleteEntry = (index) => {
+    const updatedEntries = [...entries[day]];
+    updatedEntries.splice(index, 1); // Remove the entry at the specified index
+    setEntries({ ...entries, [day]: updatedEntries });
+  };
+
+  // This equals one entry
   const renderDayEntries = (dayEntries) => {
     return dayEntries.map((entry, index) => (
       <li key={index}>
         {entry.foodName} - {entry.calories} Cal
+        <button onClick={() => deleteEntry(index)} style={{ marginLeft: '10px' }}>
+           <BsTrash3 />
+        </button>
+
       </li>
     ));
   };
@@ -48,6 +63,8 @@ const DayModal = ({ day, entries, setEntries, closeModal }) => {
           <p>No entries for this day.</p>
         )}
       </div>
+      <div>Total Calories: {totalCalories}</div>
+
       <hr />
       <div>
         <input
